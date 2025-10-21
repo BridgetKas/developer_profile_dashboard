c133a2603e321183330455c5f5a97333c85df35c
BridgetKas <bridgetnamugga@gmail.com>
2025-10-21 17:16:40 +0300

    Added routes to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import CreateProfile from "./pages/CreateProfile";
+import EditProfile from "./pages/EditProfile";
+import ProfileDetail from "./pages/ProfileDetail";
 
-function App() {
-
+export default function App() {
   return (
-    <>
-    
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<CreateProfile />} />
+        <Route path="/edit/:id" element={<EditProfile />} />
+        <Route path="/profile/:id" element={<ProfileDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
